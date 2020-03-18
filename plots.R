0c5e2c52e932adbf0347597d0d08519dcd714880
lruzzante <livio.ruzzante@unil.ch>
2020-03-18 14:51:16 +0100

    updated plots

--- plots.R
+++ plots.R
@@ -25,7 +25,7 @@ plot_all_metrics_cases <- function(data, countries, new_cases_only = FALSE) {
   
   glist <- lapply(plot_list, ggplotGrob)
   multi.page <- ggpubr::ggarrange(plotlist = glist, nrow = 2, ncol = 1) 
-  if(new_cases_only) {
+  if(new_cases_only == TRUE) {
     ggpubr::ggexport(multi.page, filename = "figures/covid-19-new-cases.pdf")
   } else ggpubr::ggexport(multi.page, filename = "figures/covid-19-total-cases.pdf")
   
@@ -36,11 +36,11 @@ plot_all_metrics_cases <- function(data, countries, new_cases_only = FALSE) {
 # High-level function to plot data over time for several countries directly from global data
 plot_cases <- function(data, countries, metric = "confirmed", logtrans = FALSE, fit_line = TRUE, new_cases_only = FALSE) {
 
-    if (new_cases_only) {
+    if (new_cases_only == TRUE) {
       data <- get_country_data_new_cases(data, countries, metric = metric)
     } else data <- get_country_data(data, countries, metric = metric)
     
-    new_cases <- draw_cases(data, metric = metric, logtrans = logtrans, fit_line = fit_line)
+    new_cases <- draw_cases(data, metric = metric, logtrans = logtrans, fit_line = fit_line, new_cases_only = new_cases_only)
 
     return(new_cases)
 
@@ -50,14 +50,14 @@ plot_cases <- function(data, countries, metric = "confirmed", logtrans = FALSE, 
 # Low level function to generate time series plot for multiple countries
 draw_cases <- function(data, metric = "confirmed", logtrans = FALSE, fit_line = FALSE, new_cases_only = FALSE) {
   
-  if (new_cases_only) {
+  if (new_cases_only == TRUE) {
     y_axis_title <- paste0("Number of new ", metric)
   } else y_axis_title <- paste0("Number of ", metric)
   
   if (metric %in% c("recovered", "confirmed")) y_axis_title <- paste0(y_axis_title, " cases")
   if (logtrans == TRUE) y_axis_title <- paste0(y_axis_title, " (log2)")
   
-  if (new_cases_only)  {
+  if (new_cases_only == TRUE)  {
     g <- ggplot(data, aes(x = Date, y = New, color = Country, fill = Country))
   } else g <- ggplot(data, aes(x = Date, y = Cases, color = Country, fill = Country))
     
